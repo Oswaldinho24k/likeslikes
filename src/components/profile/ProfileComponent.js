8355164749f147e3bab0404727a85716466c147c
Oswaldinho <oswalfut_96@hotmail.com>
2018-02-15 15:38:06 -0600

    ya

--- src/components/profile/ProfileComponent.js
+++ src/components/profile/ProfileComponent.js
@@ -4,17 +4,20 @@ const ProfileComponent = ({username, uid, image, email, descripcion, instagram, 
     return (
         <div className='profile-container'>
             <img src={image} alt={username}/>
-            <div>
+            <div className='container'>
                 <h2>{username}</h2>
+
                 <section>
                     <p className='like'> 😏 {likes?Object.keys(likes).length:0} </p>
                     <p className='dislike'>  🙄 {dislikes?Object.keys(dislikes).length:0} </p>
                 </section>
 
-                <textarea placeholder={'Tu descripción'} name={'descripcion'} onChange={handleText} value={descripcion?descripcion:''}/>
-                <input type='text' placeholder={'email'} name={'email'} onChange={handleText} value={email?email:''}/>
-                <input type='text' placeholder={'instagram'} name={'instagram'} onChange={handleText} value={instagram?instagram:''} />
-                <input type='text' placeholder={'github'} name={'github'} onChange={handleText} value={github?github:''}/>
+                <div className={"input-container"}>
+                    <textarea placeholder={'Tu descripción'} name={'descripcion'} onChange={handleText} value={descripcion?descripcion:''}/>
+                    <input type='text' placeholder={'email'} name={'email'} onChange={handleText} value={email?email:''}/>
+                    <input type='text' placeholder={'instagram'} name={'instagram'} onChange={handleText} value={instagram?instagram:''} />
+                    <input type='text' placeholder={'github'} name={'github'} onChange={handleText} value={github?github:''}/>
+                </div>
 
                 <span onClick={saveUser} >Editar</span>
                 <span onClick={logOut}>Salir</span>
